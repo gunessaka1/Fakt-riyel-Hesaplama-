2aa86d98595655fa42c1a382cd6e3011baba75be
GÜNEŞ SAKA <98685489+gunessaka1@users.noreply.github.com>
2022-09-18 13:37:44 +0300

    Add files via upload

---
+++ Donguler/Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi  ;
+          int  faktoriyel=1;
+            int i;
+            Console.Write("sayıyı giriniz :");
+            sayi = Convert.ToInt16(Console.ReadLine());
+            while (  1 <= sayi)
+            {
+                faktoriyel = faktoriyel * sayi;
+                sayi--;
+            }
+            Console.WriteLine(faktoriyel);
+            Console.Read();
+        }
+    }
+}
